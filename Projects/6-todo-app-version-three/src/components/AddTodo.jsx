536d0423c15337d7001ac0ed699cb35731493e41
Tusher <62649759+tusher719@users.noreply.github.com>
2024-06-17 21:30:12 +0600

    Project (Use Ref)

--- Projects/6-todo-app-version-three/src/components/AddTodo.jsx
+++ Projects/6-todo-app-version-three/src/components/AddTodo.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import styles from './assets/AddTodo.module.css';
 import { IoMdAdd } from "react-icons/io";
 
 function AddTodo({onNewItem}) {
-
-    const [todoName, setTodoName] = useState('');
-    const [dueDate, setDueDate] = useState('');
-
-    const handleNameChange = (event) => {
-        setTodoName(event.target.value);
-
-    }
-
-    const handleDateChange = (event) => {
-        setDueDate(event.target.value);
-    }
+    const todoNameElement =  useRef();
+    const dueDateElement =  useRef();
 
     const handleAddBtnClicked = () => {
+        const todoName = todoNameElement.current.value;
+        const dueDate = dueDateElement.current.value;
+        todoNameElement.current.value = '';
+        dueDateElement.current.value = '';
         onNewItem(todoName, dueDate);
-        setTodoName('');
-        setDueDate('');
     }
 
     return (
         <div className="container">
             <div className={`${styles.AppHeading} row kg-row`}>
                 <div className="col-6">
-                    <input type="text" value={todoName} onChange={handleNameChange} className={styles.inputTxt} placeholder="Enter Todo Here"/>
+                    <input type="text" ref={todoNameElement} className={styles.inputTxt} placeholder="Enter Todo Here"/>
                 </div>
                 <div className="col-4">
-                    <input type="date" value={dueDate} onChange={handleDateChange} className={styles.inputTxt}/>
+                    <input type="date" ref={dueDateElement} className={styles.inputTxt}/>
                 </div>
                 <div className="col-2">
                     <button type="button" className="btn btn-success kg-button" onClick={handleAddBtnClicked}>
