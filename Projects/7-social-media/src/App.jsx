c1ceea664859dc68b5298a32afe8db723f90081d
Tusher <62649759+tusher719@users.noreply.github.com>
2024-06-22 12:18:49 +0600

    Social Media UI

--- Projects/7-social-media/src/App.jsx
+++ Projects/7-social-media/src/App.jsx
@@ -1,13 +1,25 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
 
 function App() {
-
   return (
-    <>
-    <h1>Hello World!</h1>
-    </>
-  )
+    <div className="app-container d-flex">
+      <Sidebar />
+      <div className="content flex-grow-1 d-flex flex-column">
+        <Header />
+        <div className="flex-grow-1 p-3">
+          <CreatePost />
+          <PostList />
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ Projects/7-social-media/src/components/PostList.jsx
@@ -0,0 +1,33 @@
+import Post from "./Post";
+
+const PostList = () => {
+  return (
+    <>
+      <div className="row">
+        <div className="col-md-3 mb-3">
+          <Post></Post>
+        </div>
+        <div className="col-md-3 mb-3">
+          <Post></Post>
+        </div>
+        <div className="col-md-3 mb-3">
+          <Post></Post>
+        </div>
+        <div className="col-md-3 mb-3">
+          <Post></Post>
+        </div>
+        <div className="col-md-3 mb-3">
+          <Post></Post>
+        </div>
+        <div className="col-md-3 mb-3">
+          <Post></Post>
+        </div>
+        <div className="col-md-3 mb-3">
+          <Post></Post>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PostList;
