5564918d419d0c9a540faef8ad1b2c7409accfa2
Tusher <62649759+tusher719@users.noreply.github.com>
2024-06-22 12:59:43 +0600

    sidebarButton ("Home, Create Post") => {Functional}

--- Projects/7-social-media/src/App.jsx
+++ Projects/7-social-media/src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
@@ -7,14 +8,15 @@ import CreatePost from "./components/CreatePost";
 import PostList from "./components/PostList";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
+
   return (
     <div className="app-container d-flex">
-      <Sidebar />
+      <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
       <div className="content flex-grow-1 d-flex flex-column">
         <Header />
         <div className="flex-grow-1 p-3">
-          <CreatePost />
-          <PostList />
+          {selectedTab === "Home" ? <PostList /> : <CreatePost />}
         </div>
         <Footer />
       </div>
